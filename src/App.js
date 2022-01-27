ccf8f7a214a387b02015762474857a9621fd7a6d
Hayoung Jeon <hayoungj0710@gmail.com>
2022-01-27 23:44:54 +0900

    Register 페이지로의 router 추가

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from 'components/login/Login';
 import CardDashboard from 'components/card/CardDashboard';
+import Register from 'components/register/Register';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" exact element={<Login />} />
+        <Route path="/register" exact element={<Register />} />
         <Route path="/dashboard" exact element={<CardDashboard />} />
       </Routes>
     </BrowserRouter>
