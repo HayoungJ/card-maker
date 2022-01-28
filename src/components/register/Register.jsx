11927d34b09217d5e4306888de0de56873edd135
Hayoung Jeon <hayoungj0710@gmail.com>
2022-01-28 22:48:05 +0900

    가입 성공 시 toast 메세지 보여주고 로그인 페이지로 이동
    form 내에 버튼이 두 개 이상 있지 않도록 수정
    name은 회원 정보로 굳이 필요하지 않을 것 같아서 제외

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -1,33 +1,36 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from './register.module.css';
 import app from 'service/firebase';
 import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import Toast from 'components/toast/Toast';
 
 const Register = (props) => {
   const navigate = useNavigate();
 
-  const formRef = useRef();
-  const nameRef = useRef();
   const emailRef = useRef();
   const passwordRef = useRef();
   const errorRef = useRef();
 
   const [emailError, setEmailError] = useState(false);
   const [passwordError, setPasswordError] = useState(false);
+  const [toast, setToast] = useState(false);
 
   const linkTo = (route = '') => {
     navigate(`/${route}`);
   };
 
-  const createUser = async (email, password) => {
+  const createUser = (email, password) => {
     const auth = getAuth(app);
     createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         // Signed in
-        const user = userCredential.user;
+        // const user = userCredential.user;
         // ...
-        return true;
+        setToast(true);
+        setTimeout(() => {
+          linkTo();
+        }, 1000);
       })
       .catch((error) => {
         const errorCode = error.code;
@@ -47,26 +50,19 @@ const Register = (props) => {
           default:
             console.log(errorCode);
         }
-        return false;
       });
   };
 
   const handleRegister = (event) => {
     event.preventDefault();
-    const name = nameRef.current.value;
     const email = emailRef.current.value;
     const password = passwordRef.current.value;
 
-    if (name === '' || email === '' || password === '') return;
+    if (email === '' || password === '') return;
 
     setEmailError(false);
     setPasswordError(false);
-    const register = createUser(
-      emailRef.current.value,
-      passwordRef.current.value
-    ).then(() => {
-      console.log(register);
-    });
+    createUser(emailRef.current.value, passwordRef.current.value);
   };
 
   return (
@@ -78,13 +74,7 @@ const Register = (props) => {
         </section>
         <section className={styles.register}>
           <h1 className={styles.title}>Register</h1>
-          <form ref={formRef} className={styles.form} onSubmit={handleRegister}>
-            <input
-              ref={nameRef}
-              className={styles.input}
-              type="text"
-              placeholder="name"
-            />
+          <form className={styles.form} onSubmit={handleRegister}>
             <input
               ref={emailRef}
               className={`${styles.input} ${emailError ? styles.error : ''}`}
@@ -102,19 +92,18 @@ const Register = (props) => {
               className={`${styles['error-message']} ${
                 emailError || passwordError ? styles.show : ''
               }`}
-            >
-              aa
-            </p>
+            ></p>
             <button className={styles['register-button']}>Register</button>
-            <button className={styles['login-button']} onClick={() => linkTo()}>
-              Already have an account?
-            </button>
           </form>
+          <button className={styles['login-button']} onClick={() => linkTo()}>
+            Already have an account?
+          </button>
         </section>
         <section className={styles.footer}>
           <h4 className={styles.desc}>Create and Save your Business Cards!</h4>
         </section>
       </section>
+      {toast ? <Toast message={'Register Succeed'} time={1000} /> : <></>}
     </div>
   );
 };
